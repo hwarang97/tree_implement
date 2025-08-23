f7410dba643d382431497b52866e54706f212a86
dolggul2 <dolggul2@gmail.com>
2025-08-24 00:05:47 +0900

    feat: subtreeIter
    - Set parameters of BinaryNode constructor as destructor format to handle keyword argument
    - Add subtreeIter function

--- binary_tree.js
+++ binary_tree.js
@@ -1,8 +1,18 @@
 class BinaryNode {
-    constructor(item) {
-        this.parent = null;
-        this.left = null;
-        this.right = null;
+    constructor({parent=null, left=null, right=null, item=null} = {}) {
+        this.parent = parent;
+        this.left = left;
+        this.right = right;
         this.item = item;
+    }
+
+    *subtreeIter() {
+        if (this.left) {
+            yield* this.left.subtreeIter();
+        }
+        yield this;
+        if (this.right) {
+            yield* this.right.subtreeIter();
+        }
     }
 }
